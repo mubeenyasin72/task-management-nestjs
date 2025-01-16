3c3632e20ae0aa4950bb95c2d039a85c9bea5d97
mubeenyasin72 <mubeenyasin75@gmail.com>
2025-01-16 16:25:51 +0500

    connectig with database and implement typeOrm

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,7 +9,6 @@ import {
   Patch,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
 import { CraeteTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -17,35 +16,35 @@ import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
-  @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    if (Object.keys(filterDto).length) {
-      // if we have any filters defined, call tasksService.getTasksWithFilters
-      return this.tasksService.getTasksWithFilters(filterDto);
-    } else {
-      //other wise get all tasks
-      return this.tasksService.getAllTasks();
-    }
-  }
-  @Post('/create')
-  createTask(@Body() createTaskDto: CraeteTaskDto): Task {
-    console.log(createTaskDto);
-    return this.tasksService.createTask(createTaskDto);
-  }
-  @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
-    return this.tasksService.getTaskById(id);
-  }
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    this.tasksService.deleteTask(id);
-  }
-  @Patch('/:id/status')
-  updateTaskStatus(
-    @Param('id') id: string,
-    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
-  ): Task {
-    const { status } = updateTaskStatusDto;
-    return this.tasksService.updateTaskStatus(id, status);
-  }
+  // @Get()
+  // getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  //   if (Object.keys(filterDto).length) {
+  //     // if we have any filters defined, call tasksService.getTasksWithFilters
+  //     return this.tasksService.getTasksWithFilters(filterDto);
+  //   } else {
+  //     //other wise get all tasks
+  //     return this.tasksService.getAllTasks();
+  //   }
+  // }
+  // @Post('/create')
+  // createTask(@Body() createTaskDto: CraeteTaskDto): Task {
+  //   console.log(createTaskDto);
+  //   return this.tasksService.createTask(createTaskDto);
+  // }
+  // @Get('/:id')
+  // getTaskById(@Param('id') id: string): Task {
+  //   return this.tasksService.getTaskById(id);
+  // }
+  // @Delete('/:id')
+  // deleteTask(@Param('id') id: string): void {
+  //   this.tasksService.deleteTask(id);
+  // }
+  // @Patch('/:id/status')
+  // updateTaskStatus(
+  //   @Param('id') id: string,
+  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  // ): Task {
+  //   const { status } = updateTaskStatusDto;
+  //   return this.tasksService.updateTaskStatus(id, status);
+  // }
 }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksRespository } from './task.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TasksRespository])], // Allow us to dependency, inject or inject the dependency that is tasks repository
   controllers: [TasksController],
   providers: [TasksService],
 })
