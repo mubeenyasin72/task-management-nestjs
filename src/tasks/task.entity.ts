3e2a031a05832021ece930016f4540e56d014bf6
mubeenyasin72 <mubeenyasin75@gmail.com>
2025-02-24 17:36:57 +0500

    log commited 05

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
-//This is where we define the structure of the task table and it is also called Entity
+//This is where we define the structure of the task table and it is also called Entity in nestjs
 @Entity()
 export class Task {
   @PrimaryGeneratedColumn('uuid')
